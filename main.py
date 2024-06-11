6ce66a90227607ddbda601283fd92364e91bf843
Sibdroid <ryabovglebrg@gmail.com>
2024-06-12 00:20:14 +0200

    feat: make colors df.

--- main.py
+++ main.py
@@ -18,20 +18,31 @@ def get_color(colormap: cmap, value: float, max_value: float) -> str:
     return tuple_to_hex(colormap(value))
 
 
+def make_color_df(df: pd.DataFrame) -> pd.DataFrame:
+    full_colors = []
+    for column in df.columns:
+        try:
+            colors = []
+            print(column)
+            cmap = make_cmap(COLORS[column])
+            for value in df[column]:
+                colors += [get_color(cmap, value, max(df[column]))]
+            full_colors += [colors]
+        except KeyError:
+            pass
+    color_df = pd.DataFrame(full_colors, columns=df["State"],
+                            index=[i for i in COLORS.keys()]).transpose()
+    return color_df
+
+
 def main():
     df = get_wiki_tables("https://en.wikipedia.org/wiki"
                          "/2024_European_Parliament_election_in_Germany",
                          "State")[0]
     df = df.dropna()
     df["State"] = df["State"].apply(lambda x: remove_in_parens(x))
-    for column in df.columns:
-        try:
-            print(column)
-            cmap = make_cmap(COLORS[column])
-            for value in df[column]:
-                print(get_color(cmap, value, max(df[column])))
-        except KeyError:
-            pass
+    color_df = make_color_df(df)
+    print(color_df.to_string())
 
 
 if __name__ == "__main__":
