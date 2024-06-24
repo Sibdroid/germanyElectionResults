d0d452f13a0b5c58cf7949e722fd47bb34a68f15
Sibdroid <ryabovglebrg@gmail.com>
2024-06-24 23:16:21 +0200

    feat: make a *very* barebones solution for gradients.

--- main.py
+++ main.py
@@ -40,7 +40,8 @@ def make_color_df(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def make_map(color_df: pd.DataFrame,
-             party: str) -> None:
+             party: str,
+             write_path: str) -> None:
     with open("basemap.svg", encoding="UTF-8") as file:
         header_split = """inkscape:window-maximized="1" />"""
         header, paths, footer = split_svg(file.read(), header_split)
@@ -56,30 +57,84 @@ def make_map(color_df: pd.DataFrame,
                             if """style="fill""""" not in i])
             path += " />"
             new_paths += [path]
-    with open(f"{party}-map.svg", "w", encoding="UTF-8") as file:
-        file.write(header)
-        for path in new_paths:
-            file.write(path)
-        file.write(footer)
+    with open(write_path, "w", encoding="UTF-8") as file:
+        contents = header + "".join(new_paths) + footer
+        contents = contents.replace('   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"',
+                                    '''   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:xlink="http://www.w3.org/1999/xlink"''')
+        contents = contents.replace('   id="defs22"><inkscape:perspective',
+                                    '''   id="defs22"><linearGradient
+     id="linearGradient1"
+     inkscape:collect="always"><stop
+       style="stop-color:#c3e7fa;stop-opacity:1;"
+       offset="0"
+       id="stop1" /><stop
+       style="stop-color:#11a1ee;stop-opacity:1;"
+       offset="1"
+       id="stop2" /></linearGradient><inkscape:perspective''')
+        contents = contents.replace('     id="perspective26" /></defs><sodipodi:namedview',
+                                    '''     id="perspective26" /><linearGradient
+     inkscape:collect="always"
+     xlink:href="#linearGradient1"
+     id="linearGradient2"
+     x1="14.089786"
+     y1="35.530762"
+     x2="150.08684"
+     y2="35.530762"
+     gradientUnits="userSpaceOnUse" /></defs><sodipodi:namedview''')
+        contents = contents.replace('/></svg>',
+                                    ''' /&gt;<rect
+   style="fill:url(#linearGradient2);stroke-width:4;stroke-dashoffset:2.92652;paint-order:fill markers stroke"
+   id="rect1"
+   width="135.99706"
+   height="33.080364"
+   x="14.089786"
+   y="18.99058"
+   ry="0.92131364"
+   transform="matrix(1.1029651,0,0,0.4534412,-5.5405412,1.3888888)" /><text
+   xml:space="preserve"
+   style="font-size:16px;line-height:1;font-family:Roboto;-inkscape-font-specification:Roboto;text-align:center;text-anchor:middle;fill:#000000;fill-opacity:1;stroke-width:4;stroke-dashoffset:2.92652;paint-order:fill markers stroke"
+   x="14.702385"
+   y="37.368561"
+   id="text2"
+   transform="translate(4.7741776,3.1705017)"><tspan
+     sodipodi:role="line"
+     id="tspan2"
+     x="14.702385"
+     y="37.368561">8%</tspan></text><text
+   xml:space="preserve"
+   style="font-size:16px;line-height:1;font-family:Roboto;-inkscape-font-specification:Roboto;text-align:center;text-anchor:middle;fill:#000000;fill-opacity:1;stroke-width:4;stroke-dashoffset:2.92652;paint-order:fill markers stroke"
+   x="111.18678"
+   y="39.20636"
+   id="text3"
+   transform="translate(28.067248,1.3327026)"><tspan
+     sodipodi:role="line"
+     id="tspan3"
+     x="111.18678"
+     y="39.20636">31.8%</tspan></text></svg>
+
+''')
+        file.write(contents)
+
 
 def main():
-    #df = get_wiki_tables("https://en.wikipedia.org/wiki"
-    #                     "/2024_European_Parliament_election_in_Germany",
-    #                     "State")[0]
-    #df = df.dropna()
-    #df["State"] = df["State"].apply(lambda x: remove_in_parens(x))
-    #color_df = make_color_df(df)
-    #color_df = change_df_index(color_df, NAMES)
-    #for i in COLORS.keys():
-    #    make_map(color_df, i)
+    df = get_wiki_tables("https://en.wikipedia.org/wiki"
+                         "/2024_European_Parliament_election_in_Germany",
+                         "State")[0]
+    df = df.dropna()
+    df["State"] = df["State"].apply(lambda x: remove_in_parens(x))
+    color_df = make_color_df(df)
+    color_df = change_df_index(color_df, NAMES)
+    for i in COLORS.keys():
+        make_map(color_df, i, f"generated-maps/{i}-map.svg")
     #image_paths = []
     #for party in COLORS.keys():
     #    svg_to_png(f"ready-maps/{party}-map-grad.svg",
     #               f"ready-maps/{party}-map-grad.png")
     #    image_paths += [f"ready-maps/{party}-map-grad.png"]
     #make_image_grid(image_paths, 2, 3).save("ready-maps/grid.png")
-    name = "gradient0.png"
-    generate_gradient((195, 231, 250), (17, 161, 238), 200, 1000).save(name)
+    #name = "gradient0.png"
+    #generate_gradient((195, 231, 250), (17, 161, 238), 200, 1000).save(name)
 
 
 if __name__ == "__main__":
